7246d4e6f8743da16eec5972fccf0cb3479461f5
Miguel Alexis Alvarez Flores <alexisfloress503@gmail.com>
2026-04-08 18:20:15 -0600

    finalizacion de practica

--- js/script.js
+++ js/script.js
@@ -53,3 +53,47 @@ console.log("window.screen.width: "+ window.screen.width+"px ", "window.screen.h
 console.log("window.screen.availWidth: "+ window.screen.availWidth+"px ", "window.screen.availHeight: " + window.screen.availHeight + "px");
 
 console.log("document.body.clientWidth" + document.body.clientWidth + "px document.body.clientHeight: " + document.body.clientHeight + "px")
+
+function calcular(psumando,ssumando, salida){
+    //Se capturan los elementos HTML
+    primerSumando = document.getElementById(psumando);
+    segundoSumando = document.getElementById(ssumando);
+
+    //SE REALIZA EL CALCULO
+    let calculo = (parseFloat(primerSumando.value) + parseFloat(segundoSumando.value));
+
+    //SE ASIGNA EL VALOR AL COMPO DE SALIDAD
+    document.getElementById(salida).innerHTML = calculo;
+}
+
+function CrearElemento(url = null){
+    if(url !== null ){
+        url = url
+    }
+    else{
+        url = "https://as2.ftcdn.net/jpg/01/09/00/83/220_F_109008395_OQDupHMza1V6CNOzrJwWAKlaktT4IsRW.jpg"
+    }
+
+    //SE CREA UN NUEVO ELEMENTO EN EL HTML
+    let objetoimagen = document.createElement("img");
+
+    //SE ASIGAN PROPIEDAD CON VALORES ESPECIFICOS 
+    objetoimagen.id = "imagencreada";
+    objetoimagen.src = url;
+    objetoimagen.alt = "icono sobre contrl de videojuego";
+    objetoimagen.title = "control de videojuego";
+    objetoimagen.width = 220;
+    objetoimagen.height = 165;
+
+    //SE AGREGO UN NA ESCUCAH AL EVENO CLICK;
+    objetoimagen.addEventListener('click',function(){
+        //objetoimagen.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkU5595AC-bbsoPF-IK0WzUbVj9rnAbs9lkw&s";
+        objetoimagen.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkU5595AC-bbsoPF-Ik0WzUbVj9rnAbs9lkw&s";
+
+        objetoimagen.width = 225;
+        objetoimagen.height = 225;
+    })
+
+    //SE "PEGA" O SE "AHUIERE " AL LIENZO 
+    document.getElementById('lienzo').appendChild(objetoimagen);
+}
